20331027d9b03550be2428d8ad510dceef75e85c
Pedro Henrique <lppedrohs@gmail.com>
2017-12-17 22:10:57 -0200

    Refactor searchs

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -9,7 +9,7 @@ class Subject < ApplicationRecord
   enumerize :period, in: { morning: 0, afternoon: 1, night: 2, full_time: 3 }, predicates: true
 
   scope :last_rated, -> (limit) { joins(:ratings).order("ratings.id DESC").uniq.first(4) }
-  scope :search, -> (search) { joins(:courses).where("subjects.name LIKE lower(:term) OR courses.name LIKE lower(:term)", term: "%#{search.downcase}%") }
+  scope :search, -> (search) { joins(:courses).where("subjects.name ILIKE :term OR courses.name ILIKE :term", term: "%#{search}%") }
 
   def grade
     self.ratings.average(:grade) || 0.0
