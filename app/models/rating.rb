64d266271e1cddd277803018887706cd8e84eced
Pedro Henrique <lppedrohs@gmail.com>
2017-12-17 22:18:20 -0200

    Refactor

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,5 @@ class Rating < ApplicationRecord
   belongs_to :rateable, polymorphic: true
   belongs_to :user
 
-  validates :take_again, :difficulty, :text, :grade, presence: true
+  validates :difficulty, :text, :grade, presence: true
 end
